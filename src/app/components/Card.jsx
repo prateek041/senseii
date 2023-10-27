1343004a0435eee07cf14f90c47e9b71c5144ce8
Lokeshsri11 <lokeshsri1109@gmail.com>
2023-10-27 12:18:25 +0530

    update cards
    Signed-off-by: Lokeshsri11 <lokeshsri1109@gmail.com>

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+
 export default function Card(props) {
-  const main = props.main;
-  const secondary = props.secondary;
+  const { main, secondary } = props;
+
   return (
-    <div className="card bg-slate-800 text-white flex flex-col h-96 w-64 justify-center items-center text-center rounded-lg">
-      <h1>{main}</h1>
-      <p>{secondary}</p>
+    <div className="w-full max-w-md mx-auto rounded-lg overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105">
+      <div className="relative">
+        <img src="/l.avif" alt={main} className="w-full h-5/6 object-cover" />
+        <div className="absolute top-0 left-0 w-full h-2/3 bg-black opacity-40"></div>
+        <div className="absolute top-0 left-0 w-full h-2/3 flex items-center justify-center text-white text-2xl font-semibold">
+          {main}
+        </div>
+      </div>
+      <div className="px-6 py-4">
+        <h2 className="text-xl font-semibold text-gray-800">{main}</h2>
+        <p className="text-gray-600 text-base">{secondary}</p>
+      </div>
     </div>
   );
 }
