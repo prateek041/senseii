c1ee957ee62a536fe68e52356a126d126a4275b8
Lokeshsri11 <lokeshsri1109@gmail.com>
2023-10-30 13:28:27 +0530

    update
    Signed-off-by: Lokeshsri11 <lokeshsri1109@gmail.com>

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -13,21 +13,61 @@ export default function Team() {
             Meet the <b> Engineering Team</b>
           </p>
           <div className="flex justify-center items-center w-full">
-            <PersonCard
-              name={'Prateek Singh'}
-              link={'https://www.prateeksingh.tech'}
-              image={'prateek.jpeg'}
-            />
-            <PersonCard
-              name={'Pragya Srivastava'}
-              link={'https://itspragya.netlify.app/'}
-              image={'pragya.jpeg'}
-            />
-            <PersonCard
-              name={'Lokesh Srivastava'}
-              link={'https://www.linkedin.com/in/lokesh-srivastava-8465a5213/'}
-              image={'lokesh.jpeg'}
-            />
+            
+
+
+
+          <div class="w-full max-w-sm bg-custombg-100 border border-gray-200 rounded-lg shadow dark:bg-custombg-100 dark:border-gray-700">
+    <div class="flex justify-end px-4 pt-4">
+
+    </div>
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="prateek1.png" alt=""/>
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-black">Prateek</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400"></span>
+        <div class="flex mt-4 space-x-5 md:mt-7">
+            {/* <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a> */}
+            <a href="https://www.prateeksingh.tech/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Contact</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="w-full max-w-sm bg-custombg-100 border border-gray-200 rounded-lg shadow dark:bg-custombg-100 dark:border-gray-700">
+    <div class="flex justify-end px-4 pt-4">
+   
+    </div>
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="pragya1.png" alt=""/>
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-black">Pragya</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400"></span>
+        <div class="flex mt-4 space-x-3 md:mt-7">
+            {/* <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a> */}
+            <a href="https://itspragya.netlify.app/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Contact</a>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="w-full max-w-sm bg-custombg-100 border border-gray-200 rounded-lg shadow dark:bg-custombg-100 dark:border-gray-700">
+    <div class="flex justify-end px-4 pt-4">
+   
+    </div>
+    <div class="flex flex-col items-center pb-10">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="lokesh1.png" alt=""/>
+        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-black">Lokesh</h5>
+        <span class="text-sm text-gray-500 dark:text-gray-400"></span>
+        <div class="flex mt-4 space-x-3 md:mt-7">
+            {/* <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a> */}
+            <a href="https://portfolio-lokeshsri11.vercel.app/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Contact</a>
+        </div>
+    </div>
+</div>
+
+
+
+ 
           </div>
         </div>
       </div>
