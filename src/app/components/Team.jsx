d4591f2cef3560f1ea14bbc083f31071b14e6c03
Lokeshsri11 <lokeshsri1109@gmail.com>
2023-11-01 10:40:46 +0530

    fix keyword error
    Signed-off-by: Lokeshsri11 <lokeshsri1109@gmail.com>

--- src/app/components/Team.jsx
+++ src/app/components/Team.jsx
@@ -17,55 +17,55 @@ export default function Team() {
 
 
 
-          <section class="">
-  <div class="">
+          <section className="">
+  <div className="">
       <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           {/* <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2> */}
           {/* <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p> */}
       </div> 
-      <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
-          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+      <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+          <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="prateek.jpg" alt="Bonnie Avatar"/>
+                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="prateek.jpg" alt="Bonnie Avatar"/>
               </a>
-              <div class="p-5">
-                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-black">
+              <div className="p-5">
+                  <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-black">
                       <a href="#">Prateek Singh</a>
                   </h3>
                   <div class="flex justify-center mt-4 space-x-5">
-            <a href="https://www.prateeksingh.tech" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-900 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-900 dark:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-offset-gray-700 dark:focus:ring-gray-900 transition duration-300 hover:scale-105">
+            <a href="https://www.prateeksingh.tech" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-900 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-900 dark:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-offset-gray-700 dark:focus:ring-gray-900 transition duration-300 hover:scale-105">
                 Contact
             </a>
         </div>
              
               </div>
           </div> 
-          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+          <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="pragya1.jpg" alt="Jese Avatar"/>
+                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="pragya1.jpg" alt="Jese Avatar"/>
               </a>
-              <div class="p-5">
-                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-black">
+              <div className="p-5">
+                  <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-black">
                       <a href="#">Pragya Srivastava</a>
                   </h3>
-                  <div class="flex justify-center mt-4 space-x-5">
-            <a href="https://itspragya.netlify.app/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-900 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-900 dark:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-offset-gray-700 dark:focus:ring-gray-900 transition duration-300 hover:scale-105">
+                  <div className="flex justify-center mt-4 space-x-5">
+            <a href="https://itspragya.netlify.app/" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-900 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-900 dark:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-offset-gray-700 dark:focus:ring-gray-900 transition duration-300 hover:scale-105">
                 Contact
             </a>
         </div>
                  
               </div>
           </div> 
-          <div class="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
+          <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                  <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="lokesh1.jpg" alt="Michael Avatar"/>
+                  <img className="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="lokesh1.jpg" alt="Michael Avatar"/>
               </a>
-              <div class="p-5">
-                  <h3 class="text-xl font-bold tracking-tight text-gray-900 dark:text-black">
+              <div className="p-5">
+                  <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-black">
                       <a href="#">Lokesh Srivastava</a>
                   </h3>
-                  <div class="flex justify-center mt-4 space-x-5">
-            <a href="https://portfolio-lokeshsri11.vercel.app/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-900 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-900 dark:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-offset-gray-700 dark:focus:ring-gray-900 transition duration-300 hover:scale-105">
+                  <div className="flex justify-center mt-4 space-x-5">
+            <a href="https://portfolio-lokeshsri11.vercel.app/" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-900 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-900 dark:bg-gray-100 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-2 dark:focus:ring-offset-2 dark:focus:ring-offset-gray-700 dark:focus:ring-gray-900 transition duration-300 hover:scale-105">
                 Contact
             </a>
         </div>
